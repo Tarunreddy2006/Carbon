1c417193a49d2a6143fd8e0f0cd18faf5d740ec9
Tarun Reddy <tarunramakrishna38@gmail.com>
2026-05-23 20:14:03 +0530

    password

--- services/auth.py
+++ services/auth.py
@@ -16,10 +16,10 @@ from core.config import settings
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password[:72], hashed_password)
 
 def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 security = HTTPBearer()
 
